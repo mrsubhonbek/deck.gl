57ba1187b96fbfbcbd950d49a6b10849ca15412e
asadulloev <asadulloev@deck.lc>
2024-08-22 09:18:10 +0300

    fix: coords

--- src/App.tsx
+++ src/App.tsx
@@ -140,11 +140,11 @@ function App() {
               to: {
                 coordinates: [
                   e.coordinate[0] - drawLine.from[0] > 0
-                    ? e.coordinate[0] - 3e-5
-                    : e.coordinate[0] + 3e-5,
+                    ? e.coordinate[0] - 2e-6
+                    : e.coordinate[0] + 2e-6,
                   e.coordinate[1] - drawLine.from[1] > 0
-                    ? e.coordinate[1] - 3e-5
-                    : e.coordinate[1] + 3e-5,
+                    ? e.coordinate[1] - 2e-6
+                    : e.coordinate[1] + 2e-6,
                 ] as [number, number],
                 name: 'drawLine',
               },
